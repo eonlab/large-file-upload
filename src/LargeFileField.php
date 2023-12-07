cee067baae128ee524a521ac2cf0fab63f0ee270
eonlab <luisfsd@tecbit.eu>
2023-12-07 20:41:23 +0000

    Update LargeFileField.php

--- src/LargeFileField.php
+++ src/LargeFileField.php
@@ -14,19 +14,31 @@ class LargeFileField extends Field
         $this->group = $group;
         return $this;
     }
-    
+    // .setSavedPathField('#aetherupload-savedpath').setPreprocessRoute('/aetherupload/preprocess').setUploadingRoute('/aetherupload/uploading').setLaxMode(false)
     public function render()
     {
+
+        $this->addVariables([
+            'subdir'    => date("Ym")
+        ]);
+
         $name = str_replace('[', '\\\[', str_replace(']', '\\\]', $this->formatName($this->column)));
 
         $this->script = <<<SRC
 
         $('#{$name}-file').bootstrapFileInput();
         $('#{$name}-file').change(function(){
-             aetherupload('{$name}', this, '$this->group').success(getPath).upload('{$name}');
+             aetherupload(this, '{$name}').setGroup('$this->group').success(showResult).upload('{$name}');
         });
+
+        showResult = function () {
+            $('#result-{$name}').append(
+                '<small>Source：<span >' + this.resourceName + '</span> | Size：<span >' + parseFloat(this.resourceSize / (1000 * 1000)).toFixed(2) + ' MB ' + '</span> | File：<span >' + this.savedPath.substr(this.savedPath.lastIndexOf('_') + 1) + '</span></small>'
+            );
+        }
 
 SRC;
         return parent::render();
     }
 }
+
