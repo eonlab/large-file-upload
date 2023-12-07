e57a9ef50e3899ccb9de1e4e967c46152a4dd41d
eonlab <luisfsd@tecbit.eu>
2023-12-07 20:42:49 +0000

    Update large_file_upload.blade.php

--- resources/views/large_file_upload.blade.php
+++ resources/views/large_file_upload.blade.php
@@ -1,18 +1,16 @@
-<div class="form-group {!! !$errors->has($label) ?: 'has-error' !!}" >
-
-    <label for="{{$id}}" class="col-sm-2 control-label">{{$label}}</label>
-    <div class="col-sm-6">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        @include('admin::form.error')
-
-        <div class="controls" id="aetherupload-wrapper-{{$name}}">
-            <input type="file" id="{{$name}}-file" data-filename-placement="inside" class="file-inputs"/>
-            <div class="progress " style="height: 10px;margin-bottom: 2px;margin-top: 10px;width: 200px;">
-                <div id="{{$name}}-progressbar" style="background:blue;height:6px;width:0;"></div>
-            </div>
-            <span style="font-size:12px;color:#aaa;" id="{{$name}}-output"></span>
+@include("admin::form._header")
+      <div id="aetherupload-wrapper-{{$name}}">
+       <div class="input-group controls">
+              <meta name="csrf-token" content="{{ csrf_token() }}">
+              <input type="file" id="{{$name}}-file" data-filename-placement="inside" class="{{$class}}" name="{{$name}}" {!! $attributes !!} />
+              <div class="progress " style="height: 10px;margin-bottom:2px; margin-top:14px; width:100%">
+                <div id="{{$name}}-progressbar" style="background:rgb(0, 136, 255);height:6px;width:0;"></div>
+              </div>
+              <div id="result-{{$name}}" style="width:100%;"></div>
             <input type="hidden" name="{{$name}}" id="{{$name}}-savedpath" value="{{ old($column, $value) }}">
+              <span class="input-group-btn" id="{{$name}}-output">
+              @isset($btn){!! $btn !!}@endisset
+            </span>
         </div>
-        @include('admin::form.help-block')
     </div>
-</div>
+@include("admin::form._footer")
